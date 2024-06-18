5db2e120079d8fd295a68a0cd50ffc57cd2a84d8
oram1288 <cody.oram@keyin.com>
2024-06-18 14:07:50 -0230

    june18

--- Sprint1-Database/seaside_bites.sql
+++ Sprint1-Database/seaside_bites.sql
@@ -25,3 +25,61 @@ CREATE TABLE Order (
     total_amount DECIMAL(10, 2),
     FOREIGN KEY (customer_idustomerID) REFERENCES Customer(customer_id)
 );
+
+-- Kyle
+-- Create Order Item Table
+create table orderItem(
+	orderItemID serial not NULL,
+	quantity int not NUll,
+	price double not NULL,
+	menu_item_id int not NULL,
+	primary key(orderItemID),
+	foreign key(menu_item_id) references menu_item(menu_item_id)
+);
+
+-- Create Promtion Table
+create table promtion(
+	promtionID serial not NULL,
+	discountCode char varying(25) not NULL,
+	experationDate char varying(20) not NULL,
+	primary key(promtionID)
+);
+
+-- Create SalesReport Table
+create table salesReport(
+	salesReportId serial not NULL,
+	revenue double not null,
+	bestSellers char varying(30),
+	customerPrefences char varying(50),
+	primary key(salesReportID)		
+);
+
+-- Reagan
+-- Create DeliveryAddress Table
+CREATE TABLE DeliveryAddress (
+    AddressID SERIAL PRIMARY KEY,
+    customer_id INT ,
+    Street VARCHAR(255) NOT NULL,
+    City VARCHAR(100) NOT NULL,
+    Province VARCHAR(100) NOT NULL,
+    ZipCode VARCHAR(10) NOT NULL,
+    Country VARCHAR(100) NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
+-- Create ShoppingCart Table
+CREATE TABLE ShoppingCart (
+    CartID SERIAL PRIMARY KEY,
+    customer_id SERIAL PRIMARY KEY,
+    CreatedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    TotalAmount DECIMAL(10, 2) NOT NULL
+);
+
+-- Create Ingredients Table
+CREATE TABLE Ingredients (
+    IngredientID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    QuantityInStock DECIMAL(10, 2) NOT NULL,
+    Unit VARCHAR(50) NOT NULL
+);
+ 
